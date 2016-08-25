ade0c4b55ac22a817edc9373064475393b09410a
RookieLiu <763232952@qq.com>
2016-08-25 23:53:50 +0800

    完善DatabaseHelper类
    优化DatabaseHelper类，引入了dbUtils工具类

--- chapter2/src/main/java/org/smart4j/chapter2/helper/DatabaseHelper.java
+++ chapter2/src/main/java/org/smart4j/chapter2/helper/DatabaseHelper.java
@@ -7,15 +7,19 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smart4j.chapter2.util.PropsUtil;
 
+import com.mysql.fabric.xmlrpc.base.Params;
+
 
 public class DatabaseHelper {
 	private static final Logger  LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
 	private static final QueryRunner QUERY_RUNNER = new QueryRunner();
+	private static final ThreadLocal<Connection> CONNECTION_HOLDER = new ThreadLocal<Connection>();
 	
 	private static final String DRIVER;
 	private static final String URL;
@@ -35,38 +39,66 @@ public class DatabaseHelper {
 	}
 	
 	public static Connection getConnection(){
-		Connection conn =null;
+		Connection conn =CONNECTION_HOLDER.get();
 		try{
 			conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
 		}catch(SQLException e){
 			LOGGER.error("fail to connection mysql",e);
+		}finally{
+			CONNECTION_HOLDER.set(conn);
 		}
 		return conn;
 		
 	}
 	
-	public static void closeConnection(Connection conn){
+	public static void closeConnection(){
+		Connection conn = CONNECTION_HOLDER.get();
 		if(conn != null){
 			try{
 				conn.close();
 			}catch(SQLException e){
 				LOGGER.error("sql close fail",e);
+			}finally{
+				CONNECTION_HOLDER.set(conn);
 			}
 		}
 	}
 	/**
 	 * 查询实体列表
 	 */
-	public static<T> List<T> queryEntiryList(Class<T> entityClass,String sql,Object params){
+	public static<T> List<T> queryEntiryList(Class<T> entityClass,String sql,Object... params ){
 		List<T> entityList;
+		
 		try{
-			entityList = QUERY_RUNNER.query(getConnection(), sql, new BeanListHandler<T>(entityClass),params);
+			Connection conn  = getConnection();
+			entityList = QUERY_RUNNER.query(conn, sql, new BeanListHandler<T>(entityClass),params);
 		}catch(SQLException e){
 			LOGGER.error("query entity list failure",e);
 			throw new RuntimeException(e);
 		}finally{
 			closeConnection();
 		}
-		return null;
+		return entityList;
+	}
+	
+	/**
+	 * 
+	 * @param entityClass  查询实体
+	 * @param sql
+	 * @param params
+	 * @return
+	 */
+	public static <T> T queryEntry(Class<T> entityClass,String sql,Object... params){
+		T entity;
+		try{
+			Connection conn = getConnection();
+			entity = QUERY_RUNNER.query(conn, sql, new BeanHandler<T> (entityClass), params);
+		}catch(SQLException e){
+			LOGGER.error("query entity failure",e);
+			throw new RuntimeException(e);
+		}finally{
+			closeConnection();
+		}
+		return entity;
 	}
 }
--- chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -31,30 +31,15 @@ public class CustomerService {
 	 */
 	
 	public List<Customer> getCustomerList(){
-		Connection conn = null;
 		try{
-			List<Customer> customerList = new ArrayList<Customer>();
+			
 			String sql ="select * from customer";
-			conn =DatabaseHelper.getConnection();
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			ResultSet rs =stmt.executeQuery();
-			while(rs.next()){
-				Customer customer = new Customer();
-				customer.setId(rs.getLong("id"));
-				customer.setName(rs.getString("name"));
-				customer.setContact(rs.getString("contact"));
-				customer.setTelephone(rs.getString("telephone"));
-				customer.setEmail(rs.getString("email"));
-				customer.setRemark(rs.getString("remark"));
-				customerList.add(customer);
-			}
-			return customerList;
-		}catch(SQLException e){
-		//	LOGGER.error("execute sql failure",e);
+			
+			return DatabaseHelper.queryEntiryList(Customer.class,sql);
 		}finally{
-			DatabaseHelper.closeConnection(conn);
+			DatabaseHelper.closeConnection();
 		}
-		return null;
+		
 	}
 	
 	/**
